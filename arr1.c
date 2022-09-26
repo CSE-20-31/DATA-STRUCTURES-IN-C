008c6984ca33a664d51603c1da46529be94a1836
CSE-20-31 <95126640+CSE-20-31@users.noreply.github.com>
2022-09-26 15:38:52 +0530

    Update arr1.c

--- arr1.c
+++ arr1.c
@@ -6,26 +6,28 @@ int main()
 {
     int A[5] = {1,2,3,4,5};
     int element,i;
+    int flag=0;
 
     printf("enter the element you want to delete: ");
     scanf("%d", &element);
 
-    for(i=0; i<=5; i++)
+    for(i=0; i<5; i++)
     {
         if(element==A[i])
 
         {
                 A[i]=0;
-                printf("element found\n");
+                flag = 1;
+                printf("Element found\n");
                 break;
         }
-
-        else  {
-          printf("element not found\n");
-        }
-
-
     }
+
+    if(flag==0)
+        printf("Element not found");
+
+    for(i=0; i<5; i++)
+        printf("%d", A[i]);
 
 
     return 0;
